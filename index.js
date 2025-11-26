8a8634cad130186b7e22a609d8e1a5c240bf786e
Ir heritier htb <101990754+htb-pro@users.noreply.github.com>
2025-11-26 13:23:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const infoMember = document.querySelectorAll('.formImage');//la selection de tout les membres de la plateforme
-const navBar = document.querySelector('.toggle_bars');
-const header = document.querySelector('header');
-const nav = document.querySelector('.nav');
-navBar.addEventListener('click',()=>{
-    nav.classList.toggle('show');//l'insertion d'une class au click
-    header.classList.toggle('show');//l'insertion d'une class au click
-})
- infoMember.forEach(member=>{
+const show_form =document.querySelector('.btn_show_form');
+const member_profil = document.querySelectorAll('.member');
+
+// Dynamisme of member picture
+member_profil.forEach(member => {
     member.addEventListener('click',()=>{
-        member.classList.toggle("infoMember"); //l'insertion d'une class inforMamber au click
-     })
-})
+        member.classList.toggle('member-info');
+    })
+});
